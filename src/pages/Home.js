711af1ea70c6b01fc2ceda2b925d3b2e2e2805bc
Rafi Kadir <rafikadir2000@gmail.com>
2022-10-18 18:55:28 +0600

    home page done

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Banner from '../sections/Banner/Banner';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Banner />
+        </>
+    );
+};
+
+export default Home;
