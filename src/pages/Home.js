c0e2d3f7ffbd0fe4c0c1f7d5b73220c418a07d59
Rafi Kadir <rafikadir2000@gmail.com>
2022-10-19 14:46:46 +0600

    emergency done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Banner from '../sections/Banner/Banner';
+import Emergency from '../sections/Emergency/Emergency';
 import Services from '../sections/Services/Services';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbar />
             <Banner />
             <Services />
+            <Emergency />
         </>
     );
 };
