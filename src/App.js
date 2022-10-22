a997abd1c8d6aa6adfbd266968cc204e5aa0f33c
Rafi Kadir <rafikadir2000@gmail.com>
2022-10-23 00:13:24 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Home from './pages/Home';
 import {Routes, Route} from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
